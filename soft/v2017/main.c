6d3ed1096f9f417cc76e1a44d9be4fda06742fa6
Loïc Dauphin <astralien3000@yahoo.fr>
2017-04-18 00:05:23 +0200

    arm state test

--- soft/v2017/main.c
+++ soft/v2017/main.c
@@ -4,7 +4,70 @@
 #include "motors.h"
 #include "gyro.h"
 
+#include "feetech.h"
+#include "dynamixel.h"
+
+uint16_t state[3][5] = {
+  {323, 87, 512, 270, 130},
+  {512, 512, 512, 270, 130},
+  {512, 512, 75, 270, 130},
+};
+
 int main(void) {
+  uint8_t buffer[64];
+  uart_half_duplex_t stream;
+  
+  uart_half_duplex_params_t params = {
+    .uart = UART_DEV(1),
+    .baudrate = 1000000,
+    .dir = UART_HALF_DUPLEX_DIR_NONE,
+  };
+  
+  int ret = uart_half_duplex_init(&stream, buffer, sizeof(buffer), &params);
+  if(ret != UART_HALF_DUPLEX_OK) {
+    puts("ERROR uart_half_duplex_init");
+  }
+
+  size_t curstate = 0;
+  
+  while(1) {
+    char c = getchar();
+    getchar();
+
+    switch(c){
+    case '0':
+      if(curstate != 2){
+	curstate = 0;
+      };
+      break;
+    case '1':
+      curstate = 1;
+      break;
+    case '2':
+      if(curstate != 0){
+	curstate = 2;
+      };
+      break;
+    }
+
+    printf("char : %c\n", c);
+    printf("state : %i\n", curstate);
+    
+    for(int i = 10 ; i < 15 ; i++) {
+      feetech_t dev;
+      feetech_init(&dev, &stream, i);
+      
+      feetech_write8(&dev, SCS15_TORQUE_ENABLE, 1);
+      feetech_write16(&dev, SCS15_GOAL_POSITION, state[curstate][i-10]);
+    }
+    
+    for(int i = 15 ; i < 17 ; i++) {
+      dynamixel_t dev;
+      dynamixel_init(&dev, &stream, i);
+    }
+  }
+  
+  /*
   printf("motors_init : %i\n", motors_init());
   printf("gyro_init : %i\n", gyro_init());
 
@@ -21,6 +84,7 @@ int main(void) {
     motors_set_left(err_d + err_a);
     motors_set_right(err_d - err_a);
   }
+  */
 
   return 0;
 }
