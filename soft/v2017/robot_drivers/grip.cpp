1e54fb1fbc8252979a9138f20714c90223ba0683
Loïc Dauphin <astralien3000@yahoo.fr>
2017-05-25 03:43:21 +0200

    grip debug

--- soft/v2017/robot_drivers/grip.cpp
+++ soft/v2017/robot_drivers/grip.cpp
@@ -5,7 +5,9 @@
 static void _set(uint8_t id, uint16_t pos) {
   dynamixel_t dev;
   dynamixel_init(&dev, &ServoBus::instance().stream(), id);
-  dynamixel_write8(&dev, XL320_MAX_TORQUE, 100);
+  dynamixel_write16(&dev, XL320_MAX_TORQUE, 150);
+  dynamixel_write16(&dev, XL320_GOAL_TORQUE, 1023);
+  dynamixel_write16(&dev, XL320_GOAL_VELOCITY, 800);
   dynamixel_write8(&dev, XL320_TORQUE_ENABLE, 1);
   dynamixel_write16(&dev, XL320_GOAL_POSITION, pos);
 }
@@ -15,9 +17,11 @@ void Arm::Left::Grip::open(void) {
   _set(16, 512);
 }
 
+#define CLOSE_POS (300)
+
 void Arm::Left::Grip::close(void) {
-  _set(15, 250);
-  _set(16, 750);
+  _set(15, CLOSE_POS);
+  _set(16, 1024-CLOSE_POS);
 }
 
 void Arm::Right::Grip::open(void) {
@@ -26,6 +30,6 @@ void Arm::Right::Grip::open(void) {
 }
 
 void Arm::Right::Grip::close(void) {
-  _set(25, 250);
-  _set(26, 750);
+  _set(25, CLOSE_POS);
+  _set(26, 1024-CLOSE_POS);
 }
